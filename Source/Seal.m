0c8b8304f0bab5505740b89c84abc55b160ef411
Danny Brown <daniel.brown@tobi.com>
2014-06-24 18:21:54 -0700

    initial commit

---
+++ Source/Seal.m
@@ -0,0 +1,21 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Daniel Brown on 6/24/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+- (id)init {
+  self = [super init];
+  
+  if (self) {
+
+  }
+  
+  return self;
+}
+@end
